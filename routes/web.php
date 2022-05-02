c529c86106a51da5b61cedb0958ce7eb984061f9
Your Name <you@example.com>
2022-05-02 16:54:23 -0500

    evidencia 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,40 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Ruta evidencias de Paises
+Route::get('paises', function(){
+   $paises = [ 
+       "Colombia" =>[
+           "capital"=> "Bogotá",
+           "moneda"=>  "Peso",
+           "poblacion" => 51.6,
+           "ciudades" => [
+               "Cali",
+               "Medellin",
+               "Barranquilla"
+             ]
+           ],
+        "Ecuador" => [
+            "capital"=> "Quíto",
+            "moneda"=>  "Dolar estadounidense",
+            "poblacion" => 17.6,
+            "ciudades" => [
+                "Ibarra",
+                "Guayaquil"
+              ]
+            ],
+            "Rusia" => [
+                "capital"=> "Moscú",
+                "moneda"=>  "Rublo Ruso",
+                "poblacion" => 144.1,
+                "ciudades" => [
+                    "San Petersburgo",
+                ] 
+            ]
+       ];
+
+       //Código para mostrar la vista de paises 
+       return view('paises')
+       ->with('paises', $paises);
+});
